c4624289ca6602583ea031ebd9769182c23e00a9
Jared Palmer <jared@jaredpalmer.com>
2024-05-01 17:39:05 -0700

    Fix small Next.js x TypeScript nits

--- app/api/assistants/threads/[threadId]/actions/route.ts
+++ app/api/assistants/threads/[threadId]/actions/route.ts
@@ -1,14 +1,8 @@
-import OpenAI from "openai";
-const openai = new OpenAI();
-
-export const runtime = "nodejs";
+import { openai } from "@/app/openai";
 
 // Send a new message to a thread
-export async function POST(request, { params }) {
-  const body = await request.json();
-  const toolCallOutputs = body.toolCallOutputs;
-  const runId = body.runId;
-  const threadId = params.threadId;
+export async function POST(request, { params: { threadId } }) {
+  const { toolCallOutputs, runId } = await request.json();
 
   const stream = openai.beta.threads.runs.submitToolOutputsStream(
     threadId,
--- app/api/assistants/threads/route.ts
+++ app/api/assistants/threads/route.ts
@@ -1,10 +1,9 @@
-import OpenAI from "openai";
-const openai = new OpenAI();
+import { openai } from "@/app/openai";
 
 export const runtime = "nodejs";
 
 // Create a new thread
 export async function POST() {
   const thread = await openai.beta.threads.create();
-  return new Response(JSON.stringify({ threadId: thread.id }));
+  return Response.json({ threadId: thread.id });
 }
