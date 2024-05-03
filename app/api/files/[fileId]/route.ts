fb8ce650dc814b8e36239702e105e434c22f7648
ibigio <ilanbigio@gmail.com>
2024-05-03 19:47:27 -0400

    Tweak image styling.

--- app/api/files/[fileId]/route.ts
+++ app/api/files/[fileId]/route.ts
@@ -2,7 +2,10 @@ import { openai } from "@/app/openai";
 
 // download file by file ID
 export async function GET(_request, { params: { fileId } }) {
-  const [file, fileContent] = await Promise.all([openai.files.retrieve(fileId), openai.files.content(fileId)]);
+  const [file, fileContent] = await Promise.all([
+    openai.files.retrieve(fileId),
+    openai.files.content(fileId),
+  ]);
   return new Response(fileContent.body, {
     headers: {
       "Content-Disposition": `attachment; filename="${file.filename}"`,
