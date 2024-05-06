ea472dd81894832d193cb13178dc87234f63dcf7
Jesús Ferretti <jferrettiboke@users.noreply.github.com>
2024-05-06 12:55:18 +0200

    fix(types): type `messagesEndRef` in `useRef`

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -66,7 +66,7 @@ const Chat = ({
   const [threadId, setThreadId] = useState("");
 
   // automitcally scroll to bottom of chat
-  const messagesEndRef = useRef(null);
+  const messagesEndRef = useRef<HTMLDivElement | null>(null);
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
