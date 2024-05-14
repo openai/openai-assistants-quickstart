015fb6d7cb6c2b15edcfd0e72c486c2c28708365
robin.li <robin.li@webeye.com>
2024-05-14 15:29:57 +0800

    fix: update components rendering environments

--- app/examples/basic-chat/page.tsx
+++ app/examples/basic-chat/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import styles from "./page.module.css"; // use simple styles for demonstration purposes
 import Chat from "../../components/chat";
--- app/examples/file-search/page.tsx
+++ app/examples/file-search/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React from "react";
 import styles from "../shared/page.module.css";
 
